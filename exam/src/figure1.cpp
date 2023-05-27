d459cdc60be8dcb683b87127c6719f441ffef4dc
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2023-05-27 16:51:00 +0200

    Update figure1.cpp

--- exam/src/figure1.cpp
+++ exam/src/figure1.cpp
@@ -4,70 +4,45 @@
 //
 // Created by rasmus on 5/27/2023.
 //
-void runFirstSimulation(){
+void performSimulation(const Agent& A,const Agent& B,const Agent& C){
     auto lambda = 0.001;
     auto endTime = 1500;
     reaction r (LHS{{{"A",1}, {"C",1}}} >>= {{{"B",1}, {"C",1}}, lambda});
     std::cout << r;
     
     auto s = reaction::state{};
-
-    Agent A = Agent{"A", 100};
-    Agent B = Agent{"B", 0};
-    Agent C = Agent{"C", 1};
     s.storeOrUpdate(A.name, A.volume);
     s.storeOrUpdate(B.name, B.volume);
     s.storeOrUpdate(C.name, C.volume);
 
     ReactionNetwork q {{r},s};
     q.operator()(endTime);
+}
+
+void runFirstSimulation(){
+    const Agent A = Agent{"A", 100};
+    const Agent B = Agent{"B", 0};
+    const Agent C = Agent{"C", 1};
+    performSimulation(A,B,C);
     std::cout << "///-------------------------END OF SIMULATION-------------------------///\n";
     
 }
 void runSecondSimulation()
 {
-    auto lambda = 0.001;
-    auto endTime = 1500;
-    auto lhs = LHS{{{"A", 1}, {"C", 1}}};
-    auto rhs = RHS{{{"B", 1}, {"C", 1}}, lambda};
-    const reaction& rule = lhs >>= rhs;
-
-    auto s = reaction::state{};
-
-    Agent A = Agent{"A", 100};
-    Agent B = Agent{"B", 0};
-    Agent C = Agent{"C", 2};
-
-    s.storeOrUpdate(A.name, A.volume);
-    s.storeOrUpdate(B.name, B.volume);
-    s.storeOrUpdate(C.name, C.volume);
-
-    ReactionNetwork q{{rule}, s};
-    q.operator()(endTime);
-    
+   
+    const Agent A = Agent{"A", 100};
+    const Agent B = Agent{"B", 0};
+    const Agent C = Agent{"C", 2};
+    performSimulation(A,B,C);
     std::cout << "///-------------------------END OF SIMULATION-------------------------///\n";
 }
 
 void runThirdSimulation()
 {
-    auto lambda = 0.001;
-    auto endTime = 1500;
-    auto lhs = LHS{{{"A", 1}, {"C", 1}}};
-    auto rhs = RHS{{{"B", 1}, {"C", 1}}, lambda};
-    const reaction& rule = lhs >>= rhs;
-    auto s = reaction::state{};
-
-    Agent A = Agent{"A", 50};
-    Agent B = Agent{"B", 50};
-    Agent C = Agent{"C", 1};
-
-    s.storeOrUpdate(A.name, A.volume);
-    s.storeOrUpdate(B.name, B.volume);
-    s.storeOrUpdate(C.name, C.volume);
-
-    ReactionNetwork q{{rule}, s};
-    q.operator()(endTime);
-    
+    const Agent A = Agent{"A", 50};
+    const Agent B = Agent{"B", 50};
+    const Agent C = Agent{"C", 1};
+    performSimulation(A,B,C);
     std::cout << "///-------------------------END OF SIMULATION-------------------------///\n";
 }
 
@@ -75,6 +50,5 @@ int main(){
     runFirstSimulation();
     runSecondSimulation();
     runThirdSimulation();
-    
     return 0;
 }
