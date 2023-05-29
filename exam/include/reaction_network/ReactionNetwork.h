a28cc025ea53e65d879a3d7c70b910e1b17396ec
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2023-05-29 07:52:15 +0200

    network

--- exam/include/reaction_network/ReactionNetwork.h
+++ exam/include/reaction_network/ReactionNetwork.h
@@ -8,50 +8,26 @@
 #include <functional>
 #include "reaction/construction_rules.h"
 #include "reaction/reaction.h"
-
 class ReactionNetwork
 {
+    using state_history = reaction::state;
 private:
     double time = 0.0;
-
-public:
     std::vector<reaction> reactions;
-    reaction::state state;
-
-    ReactionNetwork(const std::initializer_list<reaction>& reactions, reaction::state& agentValues):
-        reactions(reactions), state(agentValues)
-    {};
-    friend std::ostream & operator << (std::ostream& s, const ReactionNetwork& value);
-    inline void print(const ReactionNetwork& s, reaction& fastestReaction){
-        std::cout << s << " by doing " << fastestReaction;
-    }
     
-    void operator()(double endTime){
-        while (time < endTime){
-            std::vector<std::pair<reaction, double>> validReactionTimes{};
-            for (auto& reaction :reactions) {
-                if (reaction.canBeSatisfied(state))
-                    validReactionTimes.emplace_back(reaction, reaction.compute_delay(state));
-            }
-            
-            for (auto it = validReactionTimes.begin(); it != validReactionTimes.end(); ++it) {
-                for (auto innerIt = validReactionTimes.begin(); innerIt != validReactionTimes.end() - 1; ++innerIt) {
-                    auto& lhs = *innerIt;
-                    auto& rhs = *(innerIt + 1);
+public:
+    
+    ReactionNetwork(const std::initializer_list<reaction>& reactions): reactions(reactions)
+            {
         
-                    if (lhs.second > rhs.second) {
-                        std::swap(lhs, rhs);
-                    }
-                }
-            }
-
-            
-            auto& fastestReaction = validReactionTimes.front();
-            time += fastestReaction.second;
-            fastestReaction.first.operator()(state);
-            print(*this, fastestReaction.first);
-        }
-    }
+            }; 
+    friend std::ostream & operator << (std::ostream& s, const ReactionNetwork& value);
+    
+    auto begin() { return reactions.begin(); }
+    auto end() { return reactions.end(); }
+    auto begin() const { return reactions.begin(); }
+    auto end() const { return reactions.end(); }
 };
+
 
 #endif  // LAMBDAS_REACTIONNETWORK_H
--- exam/include/simulation.cpp
+++
@@ -1,5 +0,0 @@
-﻿//
-// Created by rasmus on 5/26/2023.
-//
-
-#include "reaction_network/ReactionNetwork.h"
