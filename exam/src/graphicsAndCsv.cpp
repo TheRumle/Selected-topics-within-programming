78a60009edaf4108158095e9a3faa537810bb373
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2023-06-04 10:25:27 +0200

    Update graphicsAndCsv.cpp

--- exam/src/graphicsAndCsv.cpp
+++ exam/src/graphicsAndCsv.cpp
@@ -11,20 +11,35 @@ void simulateAndWriteCsv(ReactionNetworkSimulator& simpleSimulation, double endT
 }
 
 int main(){
-       
-    const Agent::P_Container A = Agent::CreateShared("A", 100);
+    //REQ5 START
+    const Agent::P_Container A = Agent::CreateShared("A", 100); 
     const Agent::P_Container B = Agent::CreateShared("B", 0);
     const Agent::P_Container C = Agent::CreateShared("C", 1);
     double lambda = 0.001;
-    const Agent::P_Container::Composite composite = A + C;
-    Reaction reaction{composite >>= C + B,lambda}; //alternatively, A + C is written directly in the reaction
     
-    ReactionNetwork simpleNetwork{{reaction}}; //The above is encapsulated in functions createNthSimpleSimulation
+    //REQ1 START
+    const Agent::P_Container::Composite composite = A + C; 
+    Reaction reaction{composite >>= C + B,lambda}; //alternatively, A + C is written directly in the reaction
+    //REQ1 END
+    
+    //REQ2 START
+    ReactionNetwork simpleNetwork{reaction}; //The above is encapsulated in functions createNthSimpleSimulation
+    std::cout << simpleNetwork;
+    GraphVizFactory {simpleNetwork, "simple_network"}
+        .createGraphVizPng();
+    //REQ2 END
+    
+    
+    //REQ4, 6 START
     ReactionNetworkSimulator simpleSimulator(simpleNetwork);
     AllStateCopyMonitor monitor{};
     simpleSimulator(1500, monitor); // perform the simulation
+    //REQ4 END
+    
+    //REQ 7 START
     CsvFactory csvFactory{monitor, ';'};
     csvFactory.writeStateHistoryToCsv("simple_simulation1");//Above is encapsulated in simulateAndWriteCsv
+    //REQ 7 END
     
     
     ReactionNetworkSimulator simpleSimulation2 = create2ndSimpleNetwork();
@@ -46,6 +61,9 @@ int main(){
     simulateAndWriteCsv(circadianSimulator, 100, "circadian");
     GraphVizFactory {circadianSimulator.getNetwork(), "circadian"}
         .createGraphVizPng();
+    
+    //REQ5 END 
+    
     
     return 0;
 }
